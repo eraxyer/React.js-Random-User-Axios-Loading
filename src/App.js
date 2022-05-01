7870c73a6176d0711ab90d7ed444b465a0aef626
Eray ER <eraxyer@gmail.com>
2022-05-01 15:11:23 +0300

    react.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Component, } from 'react';
+import axios from 'axios';
+import ReactLoading from 'react-loading';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  state = {
+    persons: [],
+    loading: true,
+  }
+  
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        setTimeout(() => {
+          this.setState({ persons, loading: false });
+        }, 1000)
+      })
+  }
+  
+  render() {
+    return (
+      
+      <div className="container-fluid bg-dark vh-100 vw-100 d-flex align-items-center">
+        {this.state.loading ?
+        <ReactLoading type={'bubbles'} color={'green'} height={'100%'} width={'50%'}  />
+        :
+        <div className="text-white container-fluid text-center">
+          <div className="fs-1 fw-lighter ">Random User List</div>
+            {this.state.persons.map(person =>
+              <li key={person.id}>{person.username} : {person.name}</li>
+            )}
+        </div>
+        }
+      </div>
+
+    )
+  }
 }
-
-export default App;
